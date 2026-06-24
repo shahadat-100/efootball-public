7d12ebc0b3b749ed15e5cb35d88c891a169b6334
Shahadat_Cibl <ios.shahadathossen@gmail.com>
2026-06-24 19:55:13 +0600

    feat: integrate Vercel Speed Insights for performance monitoring

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { AppProvider } from './app/providers'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppProvider />
+    <SpeedInsights />
   </React.StrictMode>,
 )
